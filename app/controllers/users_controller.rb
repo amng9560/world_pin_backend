98ed0caabb5fd8c470e05f84b9b0c25c84e0a733
Amy Nguyen <amy.nguyen730@gmail.com>
2020-01-04 14:25:40 -0700

    adds environment variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_request
+    skip_before_action :authenticate_request, except: [:show]
 
     def index
         @users = User.all
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+  # get 'validate', to: 'application#authenticate_request'
 end
